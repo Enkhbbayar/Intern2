62f6bd28a239dc2770ff4347a0bfad72ff6cbfb5
enh.bayr.549 <enh.bayr.549@gmail.com>
2024-09-10 16:35:23 +0800

    Fix axios import and configuration

--- vite.config.mjs
+++ vite.config.mjs
@@ -10,11 +10,6 @@ import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ["axios"],
-    },
-  },
   plugins: [
     Vue({
       template: { transformAssetUrls },
