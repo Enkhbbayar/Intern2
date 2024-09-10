b6e9ebb67cbaa75722443eb1e73c4b9d29e0b644
enh.bayr.549 <enh.bayr.549@gmail.com>
2024-09-10 16:51:47 +0800

    Fix axios import and configuration

--- src/main.js
+++ src/main.js
@@ -5,28 +5,26 @@
  */
 
 // Plugins
-import { registerPlugins } from '@/plugins'
+import { registerPlugins } from "@/plugins";
 
 // Components
-import App from './App.vue'
+import App from "./App.vue";
 
 // Composables
-import { createApp } from 'vue'
-import axios from "axios";
+import { createApp } from "vue";
+import axios from 'axios';
 import VueAxios from "vue-axios";
-import vuetify from './plugins/vuetify';
+import vuetify from "./plugins/vuetify";
 // import { toast, type ToastOptions } from "vue3-toastify";
 import moment from "moment";
 import CountryFlag from "vue-country-flag-next";
 
-const app = createApp(App)
+const app = createApp(App);
 
-axios.defaults.baseURL =
-"https://api.openbox.mn/api";
+axios.defaults.baseURL = "https://api.openbox.mn/api";
 
-registerPlugins(app)
+registerPlugins(app);
 
 app.use(VueAxios, axios);
 
-app.mount('#app')
-
+app.mount("#app");
